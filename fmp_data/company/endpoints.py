f5a72ed36addd2ecc29a2129235416c827c8da97
Mehdi Zare <mehdizarem@users.noreply.github.com>
2025-07-29 17:54:33 -0400

    Update `HISTORICAL_PRICE` endpoint path to use simplified URL

--- fmp_data/company/endpoints.py
+++ fmp_data/company/endpoints.py
@@ -82,7 +82,7 @@ SIMPLE_QUOTE: Endpoint = Endpoint(
 
 HISTORICAL_PRICE: Endpoint = Endpoint(
     name="historical_price",
-    path="historical-price-eod/light/light",
+    path="historical-price-eod/light",
     version=APIVersion.STABLE,
     description="Get historical daily price data",
     mandatory_params=[
